216d5c832df5b1146e0eb5e30140b94af707a3e1
Nic Robinson <nicholas.robinson@lokalise.com>
2025-11-14 11:59:22 +0000

    Set up a typescript runtime

---
+++ 2023/TypeScript/day1/solution.ts
@@ -0,0 +1,7 @@
+export function parse() {
+
+}
+
+export function partOne() {
+
+}
---
+++ TS-core/main.ts
@@ -0,0 +1,230 @@
+import { createInterface, Interface } from "node:readline/promises";
+import * as fs from "node:fs";
+import { run } from "node:test";
+
+const CASE_INTRO = "--- AOC "
+const CASE_END = "--- END"
+
+let cli: Interface | undefined
+
+let year: string | undefined,
+    day: string | undefined,
+    part: 1 | 2 = 1;
+
+let solution: (<T>(input: T) => void) | undefined,
+    parser: (<T>(raw: string) => T) | undefined,
+    runningInput: string | undefined = undefined,
+    runningInputFile: string | undefined = undefined
+
+while (true) {
+    await tick()
+}
+
+async function tick() {
+    // Clear the terminal
+    process.stdout.write('\x1Bc')
+
+    if (cli) cli.close();
+    cli = createInterface({ input: process.stdin, output: process.stdout });
+
+    if (year === undefined) return initialiseYear(cli)
+    if (day === undefined) return initialiseDay(cli)
+    if (![1, 2].includes(part)) return initialisePart(cli)
+
+    await readSolution()
+    await readInput()
+    runSolution()
+
+    printInstructions(year, day);
+
+    await Promise.race([
+        watchForChanges(),
+        listenForCommands(cli),
+    ])
+}
+
+function parsePart(part: string | undefined) {
+    return [1, 2].includes(Number(part)) ? Number(part) as 1 | 2 : undefined;
+}
+
+async function initialiseYear(cli: Interface) {
+    const answer = await cli.question("Run (year [day] [part]): ");
+    const [y, d, p] = answer.split(" ");
+    if (!y) return initialiseYear(cli)
+
+    year = y;
+    day = d;
+
+    const parsed = parsePart(p)
+    if (parsed) part = parsed;
+}
+
+async function initialiseDay(cli: Interface) {
+    day = await cli.question("Run day: ");
+}
+
+async function initialisePart(cli: Interface) {
+    const answer = parsePart(await cli.question("Run part (1 or 2): "));
+    if (!answer) return initialisePart(cli);
+    part = answer
+}
+
+function printInstructions(year: string, day: string) {
+    console.log(`🚀 Running :: Day ${day}, ${year} | Part ${part}`);
+
+    console.log()
+    const suffix = ['', 'One', 'Two'][part];
+    if (parser) console.log(`✅  Found parser`);
+    else console.log(`⚠️  Could not find a parser called: parse or parsePart${suffix}`)
+    if (solution) console.log(`✅  Loaded solution: part${suffix}`);
+    else console.log(`⚠️  Could not find a solution called: part${suffix}`)
+    if (runningInput !== undefined) console.log(`✅  Running input: ${runningInputFile}`);
+    else console.log(`⚠️  No input.txt or input.part${part}.txt found`)
+
+    console.log()
+    console.log("Available commands")
+    if (part === 1) console.log("• toggle \t=> Activate part 2")
+    if (part === 2) console.log("• toggle \t=> Activate part 1")
+    console.log("• run \t\t=> Run solution")
+    console.log("• part [number] => Set the active part")
+    console.log("• day  [number] => Set the active day")
+    console.log("• year [number] => Set the active year")
+}
+
+async function listenForCommands(cli: Interface) {
+    console.log()
+    const [answer, command] = (await cli.question("Command: ")).split(" ");
+
+    switch (answer) {
+        case "toggle": {
+            part = [1, 2].find(p => p !== part) as 1 | 2;
+            break;
+        }
+        case "run": {
+            // Doing nothing will cause tick and rerun active solution
+            break;
+        }
+        case "part": {
+            const parsed = parsePart(command);
+            if (!parsed) {
+                console.log(`❌  Part can only be 1 or 2 - but got ${command}`)
+                break;
+            }
+            part = parsed;
+            break
+        }
+        case "day": {
+            day = command
+            break;
+        }
+        case "year": {
+            year = command
+            break;
+        }
+        default: {
+            console.log()
+            console.log(`❌  I don't know what to do with "${answer}" - try again`)
+            return listenForCommands(cli)
+        }
+    }
+}
+
+async function watchForChanges() {
+    // const solutionPath = `../${year}/TypeScript/day${day}/solution.ts`
+    // const inputPaths = [
+    //     `../${year}/TypeScript/day${day}/input.part${part}.txt`,
+    //     `../${year}/TypeScript/day${day}/input.txt`,
+    // ]
+
+
+    return new Promise<void>(resolve => {
+        try {
+            fs.watch(`../${year}/TypeScript/day${day}`, () => resolve())
+        } catch (e) {
+            console.log(`Could not find a solution for ${year}/TypeScript/day${day}`)
+            // Wait forever
+        }
+    })
+}
+
+async function readSolution() {
+    try {
+        const module = await import(`../${year}/TypeScript/day${day}/solution.ts`);
+        if (!part || !day || !year) return;
+
+        const suffix = ['', 'One', 'Two'][part];
+
+        parser = module[`parsePart${suffix}`] ?? module[`parse`];
+        solution = module[`part${suffix}`]
+
+    } catch (err) {
+        solution = undefined;
+        parser = undefined;
+    }
+}
+
+async function readInput() {
+    try {
+        let fileName = `input.part${part}.txt`
+        runningInput = readInputFileContents(fileName);
+
+        if (runningInput === undefined) fileName = `input.txt`;
+        runningInput = readInputFileContents(fileName);
+
+        runningInputFile = runningInput === undefined ? undefined : fileName;
+    } catch (err) {
+        runningInput = undefined;
+        runningInputFile = undefined;
+    }
+}
+
+function readInputFileContents(fileName: string) {
+    try {
+        return fs.readFileSync(`../${year}/TypeScript/day${day}/${fileName}`).toString();
+    } catch (err) {
+        return undefined;
+    }
+}
+
+type CaseDetails = {
+    part: 1 | 2;
+    name: string;
+    lines: string[];
+}
+
+function runSolution() {
+    if (!runningInput || !solution || !parser || !part) return;
+    const cases = loadCases(runningInput)
+
+    console.log(cases)
+
+}
+
+function loadCases(raw: string) {
+    const cases: CaseDetails[] = [];
+
+    let caseDetails: CaseDetails = { name: "", part, lines: [] };
+    for (let line of raw.split("\n")) {
+        if (line.startsWith(CASE_INTRO)) {
+            const [p, name] = line.slice(CASE_INTRO.length).split(" | ");
+            caseDetails.part = parsePart(p) ?? part;
+            caseDetails.name = name ?? "Input";
+            caseDetails.lines = []
+
+        } else if (line.startsWith(CASE_END)) {
+            cases.push(caseDetails);
+            caseDetails = { name: "", part, lines: [] }
+
+        } else {
+            caseDetails.lines.push(line);
+        }
+    }
+
+    if (caseDetails.lines.length > 1) {
+        caseDetails.name ??= "Default case"
+        caseDetails.part ??= 1;
+        cases.push(caseDetails);
+    }
+
+    return cases;
+}
