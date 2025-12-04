073f927c6bce8e200bdf8a11de65fe5471002244
Nic Robinson <nicholas.robinson@lokalise.com>
2025-12-04 09:15:21 +0000

    2025 Day 4 cleaned

--- 2025/TypeScript/day4/solution.ts
+++ 2025/TypeScript/day4/solution.ts
@@ -1,71 +1,48 @@
-type Input = string[][]
-type NeighbourMap = number[][]
+type Input = boolean[][]
+
+const ROLL = '@'
 
 export function parse(raw: string): Input {
-    return raw.split('\n').map(line => line.split(''))
+    return raw.split('\n').map(line => line.split('').map(c => c === ROLL))
 }
 
 export function partOne(input: Input) {
-    return cleanHouse(mapNeighbours(input), input)
+    return cleanHouse(input)
 }
 
 export function partTwo(input: Input) {
-    const map = mapNeighbours(input)
-
-    let cleaned: number, totalCleaned = cleaned = cleanHouse(map, input)
+    let cleaned: number, totalCleaned = cleaned = cleanHouse(input)
     while (cleaned > 0) {
-        totalCleaned += cleaned = cleanHouse(map, input)
+        totalCleaned += cleaned = cleanHouse(input)
     }
 
     return totalCleaned
 }
 
-function mapNeighbours(grid: Input): NeighbourMap {
-    const map = Array.from({ length: grid.length }, () => Array(grid[0]!.length).fill(0))
-
-    for (let y = 0, yEnd = grid.length; y < yEnd; ++y) {
-        for (let x = 0, xEnd = grid[0]!.length; x < xEnd; ++x) {
-            map[y]![x] = countNeighbouringRolls(grid, x, y)
+function cleanHouse(input: Input) {
+    let toRemove: [number, number][] = []
+    for (let y = 0, yEnd = input.length; y < yEnd; ++y) {
+        for (let x = 0, xEnd = input[0]!.length; x < xEnd; ++x) {
+            if (input[y]?.[x] && countNeighbouringRolls(input, x, y) < 4) toRemove.push([x, y])
         }
     }
 
-    return map as NeighbourMap
+    for (let [x, y] of toRemove) {
+        input[y]![x] = false
+    }
+
+    return toRemove.length
 }
 
 function countNeighbouringRolls(grid: Input, x: number, y: number) {
-    if (grid[y]![x] != '@') return 0
+    if (!grid[y]![x]) return 0
 
-    return +(grid[y + 1]?.[x + 1] == '@') +
-        +(grid[y + 1]?.[x] == '@') +
-        +(grid[y + 1]?.[x - 1] == '@') +
-        +(grid[y]?.[x + 1] == '@') +
-        +(grid[y]?.[x - 1] == '@') +
-        +(grid[y - 1]?.[x + 1] == '@') +
-        +(grid[y - 1]?.[x] == '@') +
-        +(grid[y - 1]?.[x - 1] == '@')
-}
-
-function cleanHouse(map: NeighbourMap, input: Input) {
-    let cleaned = 0
-    for (let y = 0, yEnd = map.length; y < yEnd; ++y) {
-        for (let x = 0, xEnd = map[0]!.length; x < xEnd; ++x) {
-            if (input[y]![x]! === '@' && map[y]![x]! != null &&  map[y]![x]! < 4) {
-                map[y + 1]?.[x + 1] && map[y + 1]![x + 1]!--;
-                map[y + 1]?.[x] && map[y + 1]![x]!--;
-                map[y + 1]?.[x - 1] && map[y + 1]![x - 1]!--;
-                map[y]?.[x + 1] && map[y]![x + 1]!--;
-                map[y]?.[x - 1] && map[y]![x - 1]!--;
-                map[y - 1]?.[x + 1] && map[y - 1]![x + 1]!--;
-                map[y - 1]?.[x] && map[y - 1]![x]!--;
-                map[y - 1]?.[x - 1] && map[y - 1]![x - 1]!--;
-
-                map[y]![x] = 0;
-                input[y]![x]! = '.';
-
-                cleaned++;
-            }
-        }
-    }
-
-    return cleaned
+    return +(grid[y + 1]?.[x + 1] == true) +
+        +(grid[y + 1]?.[x] == true) +
+        +(grid[y + 1]?.[x - 1] == true) +
+        +(grid[y]?.[x + 1] == true) +
+        +(grid[y]?.[x - 1] == true) +
+        +(grid[y - 1]?.[x + 1] == true) +
+        +(grid[y - 1]?.[x] == true) +
+        +(grid[y - 1]?.[x - 1] == true)
 }
